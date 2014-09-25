d3f50f9309aa288a1991f88d4f10e4ac56c37ce3
Brian Hall <bhall@goimage.com>
2014-09-25 11:06:51 -0400

    Added UserCache class and ListExtensions class.

---
+++ RedisSandbox.Console/Core/ExtensionMethods/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RedisSandbox.Console.Core.ExtensionMethods
+{
+    public static class ListExtensions
+    {
+        public static List<List<T>> ToBatch<T>(this List<T> currentList, int batchSize)
+        {
+            var batchList = new List<List<T>>();
+            var maxBatchCount = currentList.Count < batchSize ? currentList.Count : batchSize;
+            var currentCount = 0;
+            while (currentCount < currentList.Count)
+            {
+                var batch = new List<T>();
+                batch.AddRange(currentList.Skip(currentCount).Take(maxBatchCount).ToList());
+                batchList.Add(batch);
+                currentCount += maxBatchCount;
+            }
+            return batchList;
+        }
+    }
+}
---
+++ RedisSandbox.Console/Identity/Cache/UserCache.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RedisSandbox.Console.Identity.Cache
+{
+    public class UserCache
+    {
+        private readonly IAppCache _appCache;
+
+        public UserCache() { _appCache = _appCache ?? new RedisCache(); }
+        
+        public UserCache (IAppCache appCache): this() { _appCache = appCache; }
+
+        public User GetById(int id) { return _appCache.GetAllIndexedItemsInCache<User>(ComposeIndexKey()).FirstOrDefault(usr => usr.Id == id); }
+
+        public User GetByUserName(string userName)
+        {
+            return _appCache.GetValue<User>(ComposeKey(userName), ComposeIndexKey());
+        }
+
+        public void PutUserInCache(User user)
+        {
+            _appCache.Put(ComposeKey(user.Username), user, TimeSpan.FromDays(30), ComposeIndexKey());
+        }
+
+        public void RemoveUserFromCache(User user) { _appCache.Remove(ComposeKey(user.Username), ComposeIndexKey()); }
+
+        public IEnumerable<User> GetAllUsersInCache() { return _appCache.GetAllIndexedItemsInCache<User>(ComposeIndexKey()); }
+
+        public User GetByEmail(string emailAddress)
+        {
+            return GetAllUsersInCache().FirstOrDefault(usr => usr.Emails.Any(eml => eml.EmailAddress == emailAddress));
+        }
+
+        #region Key Composers
+
+        private static string ComposeKey(string keyItem) { return string.Format("{0}_{1}", "UserEntity", keyItem); }
+
+        private static string ComposeIndexKey() { return string.Format("{0}", "UsersIndexList"); }
+
+        private static string ComposeChangePasswordKey(string keyItem) { return string.Format("{0}_{1}", "ChangePasswordToken", keyItem); }
+
+        private static string ComposeChangePasswordIndexKey() { return "ChangePasswordTokensList"; }
+
+
+        #endregion
+    }
+}
